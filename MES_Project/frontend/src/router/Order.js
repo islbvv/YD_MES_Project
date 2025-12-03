2d9923fa06144023032e148200df9dca25b10786
scv2780 <tjdcksgur1@naver.com>
2025-12-03 10:55:28 +0900

    성찬혁/출고조회,출고관리/25-12-03

--- MES_Project/frontend/src/router/Order.js
+++ MES_Project/frontend/src/router/Order.js
@@ -3,5 +3,15 @@ export default [
         path: '/',
         name: 'dashboard',
         component: () => import('@/views/Dashboard.vue')
+    },
+    {
+        path: '/ForwardingManagement',
+        name: 'ForwardingManagement',
+        component: () => import('@/views/release/ForwardingManagement.vue')
+    },
+    {
+        path: '/ForwardingCheck',
+        name: 'ForwardingCheck',
+        component: () => import('@/views/release/ForwardingCheck.vue')
     }
 ];
