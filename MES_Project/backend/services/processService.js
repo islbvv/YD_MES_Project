a438880e6dce115c0a464f55ce7d5410cd71b8d3
doowoo <itomato1369@gmail.com>
2025-12-08 15:10:28 +0900

    김동우/라인정보/25-12-08

--- MES_Project/backend/services/processService.js
+++ MES_Project/backend/services/processService.js
@@ -28,6 +28,15 @@ const createProcessFlowBindingValues = (params) => {
   return values;
 };
 
+// 바인딩 값 변환 로직 내부 함수
+const createListFlowBindingValues = (params) => {
+  // Vue에서 받은 검색 조건 객체의 값을 추출 값이 없으면 null
+  const pc = params.lineCode || null;
+  // SQl 템플릿의 ? 순서에 맞춰 값을 나열
+  const values = [pc, pc];
+  return values;
+};
+
 // 공정 흐름도 목록 가져오기
 exports.getProcessFlowList = async (searchParams) => {
   const sqlAlias = "getProcessFlowList";
@@ -52,6 +61,48 @@ exports.getSubProcessList = async (processCode) => {
     return rows;
   } catch (err) {
     console.error("processService.js흐름도상세오류", err.message);
+    throw err;
+  }
+};
+
+// 공정 코드 상세 정보
+exports.getSubProcessCode = async (poCode) => {
+  const sqlAlias = "getSubProcessCode";
+  const values = [poCode];
+
+  try {
+    const rows = await query(sqlAlias, values);
+    return rows;
+  } catch (err) {
+    console.error("processService.js공정코드 상세 오류", err.message);
+    throw err;
+  }
+};
+
+// 라인 흐름도 목록 가져오기
+exports.getLineFlowList = async (searchParams) => {
+  const sqlAlias = "getLineFlowList";
+  const values = createListFlowBindingValues(searchParams);
+
+  try {
+    const rows = await query(sqlAlias, values);
+    return rows;
+  } catch (err) {
+    console.error("processService.js 라인흐름도 오류", err.message);
+    throw err;
+  }
+};
+
+// 라인 상세 공정 가져오기
+exports.getLineDetailList = async (lineCode) => {
+  const sqlAlias = "getLineDetailList";
+  const values = [lineCode];
+
+  try {
+    const rows = await query(sqlAlias, values);
+    return rows;
+  } catch (err) {
+    console.error("processService.js 라인 상세정보 오류", err.message);
     throw err;
   }
 };
